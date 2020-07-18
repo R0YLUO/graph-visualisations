f538ded67295d2ece20efa3cf8c4059be741ca84
Roy Luo <r.luo2418@gmail.com>
2020-07-18 12:58:04 +1200

    creating window for visualising a binary tree

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-from PIL import Image, ImageTk
 import tkinter as tk 
-import os
+from visualise_binary_tree import BinaryTreeWindow
 
 
 class GraphVisualisingApplication: 
@@ -8,27 +7,31 @@ class GraphVisualisingApplication:
     def __init__(self, master: tk.Tk) -> None:
         bg_colour = "#263D42"
         self.master = master
+        self.master.title("Graph visualisation tool")
         self.master.configure(bg=bg_colour)
         self.master.geometry("800x600")
         self.title = tk.Label(master=self.master, bg=bg_colour, 
                               text="Welcome to the graph visualisation tool", fg="orange", font=("Helvetica", 40))
         self.title.pack()
-        self.binary_tree_button = tk.Button(master=self.master, text="Binary Search Tree", command=self.run_binary_tree)
+        self.binary_tree_button = tk.Button(master=self.master, text="Binary Search Tree", command=self.__run_binary_tree)
         self.binary_tree_button.pack() 
-        self.heap_button = tk.Button(master=self.master, text="Max Heap", command=self.run_heap)
+        self.heap_button = tk.Button(master=self.master, text="Max Heap", command=self.__run_heap)
         self.heap_button.pack()
-
-    def run_binary_tree(self): 
-        print("runninb  tree")
-
-    def run_heap(self): 
-        print("running heap")
-    
-    def run(self) -> None: 
         self.master.mainloop()
 
+    def __run_binary_tree(self) -> None: 
+        app_root = tk.Tk()
+        app = BinaryTreeWindow(app_root) 
+        app.run()
+
+    def __run_heap(self) -> None: 
+        print("running heap")
+
+    def run(self) -> None: 
+        self.master.mainloop()
+    
 
 if __name__ == "__main__": 
-    root = tk.Tk()
+    root = tk.Tk() 
     app = GraphVisualisingApplication(root)
-    app.run() 
+    app.run()
--- visualise_binary_tree.py
+++ visualise_binary_tree.py
@@ -1,2 +1,29 @@
 from binary_tree import BinarySearchTree 
+from PIL import Image, ImageTk
+import os
+import tkinter as tk
+from image import get_number_images
 
+
+class BinaryTreeWindow: 
+
+    def __init__(self, master: tk.Tk) -> None: 
+        self.master = master
+        self.master.geometry("800x600")
+        self.master.title("Visualise binary serach tree")
+        self.canvas = tk.Canvas(master=self.master, bg="blue", width=800, height=450)
+        self.canvas.pack()
+        self.number_buttons_frame = tk.Frame(master=self.master, width=800, height=100)
+        self.number_buttons_frame.pack()
+        self.number_images = get_number_images(self.master)
+        self.__display_number_buttons()
+    
+    def __display_number_buttons(self) -> None: 
+        for row in range(2): 
+            for col in range(5): 
+                button = tk.Button(master=self.number_buttons_frame) 
+                button.config(image=self.number_images[row*5+col])
+                button.grid(row=row, column=col)
+
+    def run(self) -> None: 
+        self.master.mainloop()
