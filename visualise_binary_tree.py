c1490b2fe62343e12e847ff8eba58747d664095b
Roy Luo <r.luo2418@gmail.com>
2020-07-20 22:43:15 +1200

    implementing dynamic line length to fit graph

--- visualise_binary_tree.py
+++ visualise_binary_tree.py
@@ -13,8 +13,8 @@ class BinaryTreeWindow:
         self.master = master
         self.master.geometry(str(self.WIN_WIDTH) + 'x' + str(self.WIN_HEIGHT))
         self.master.title("Visualise binary serach tree")
-        self.canvas = tk.Canvas(master=self.master, width=self.WIN_WIDTH, height=450)
-        self.canvas.pack()
+        self.canvas = tk.Canvas(master=self.master, height=450)
+        self.canvas.pack(fill=tk.BOTH)
         self.number_buttons_frame = tk.Frame(master=self.master, width=self.WIN_WIDTH, height=150)
         self.number_buttons_frame.pack()
         self.number_images = get_number_images(self.master, (50,50))
@@ -51,16 +51,18 @@ class BinaryTreeOnCanvas:
 
     def insert(self, number: int, button: tk.Button): 
         button.destroy()
-        self.root = self.__insert_aux(number, self.root, 600, 20) 
+        self.root = self.__insert_aux(number, self.root, 600, 20, 350) 
 
-    def __insert_aux(self, number: int, current: BinaryTreeNode, x_coord: int, y_coord: int, previous_coords: tuple = None) -> BinaryTreeNode: 
+    def __insert_aux(self, number: int, current: BinaryTreeNode, x_coord: int, y_coord: int, line_length: int, previous_coords: tuple = None) -> BinaryTreeNode: 
+        if line_length < 50: 
+            line_length = 50
         if current is None: 
             current = BinaryTreeNode(number) 
             self.__update_canvas(x_coord, y_coord, self.number_images[number - 1], previous_coords)
         elif number > current.value: 
-            current.right_child = self.__insert_aux(number, current.right_child, x_coord + 100, y_coord + 40, (x_coord, y_coord))
+            current.right_child = self.__insert_aux(number, current.right_child, x_coord + line_length, y_coord + 40, line_length - 160, (x_coord, y_coord))
         elif number < current.value: 
-            current.left_child = self.__insert_aux(number, current.left_child, x_coord - 100, y_coord + 40, (x_coord, y_coord)) 
+            current.left_child = self.__insert_aux(number, current.left_child, x_coord - line_length, y_coord + 40, line_length - 160, (x_coord, y_coord)) 
         return current
 
     def __update_canvas(self, x_coord, y_coord, img, previous_coords: tuple): 
