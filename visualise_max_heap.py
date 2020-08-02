dbc00dd32df9f2ab09aadbeef8ad7c0978996aec
Roy Luo <r.luo2418@gmail.com>
2020-08-02 22:56:21 +1200

    implementing rising of newly added nodes to correct positions

--- visualise_max_heap.py
+++ visualise_max_heap.py
@@ -38,24 +38,46 @@ class MaxHeapWindow:
 
 class NumberOnCanvas: 
 
-    def __init__(self, number, image, x_coord, y_coord):
+    def __init__(self, number, image, canvas: tk.Canvas, x_coord, y_coord, coords_of_parent=None):
         self.number = number
         self.image = image 
         self.x_coord = x_coord
         self.y_coord = y_coord 
+        self.canvas = canvas
+        self.canvas_image = None
         self.line_to_parent = None
-        self.coords_of_parent = None
+        self.coords_of_parent = coords_of_parent
+        self.add_to_canvas()
     
-    def add_to_canvas(self, canvas: tk.Canvas, coords_of_parent=None): 
-        canvas.create_image(self.x_coord, self.y_coord, image=self.image)
-        if coords_of_parent is not None: 
-            self.line_to_parent = canvas.create_line(coords_of_parent[0], coords_of_parent[1], self.x_coord, self.y_coord)
+    def add_to_canvas(self): 
+        self.canvas_image = self.canvas.create_image(self.x_coord, self.y_coord, image=self.image)
+        if self.coords_of_parent is not None: 
+            self.line_to_parent = self.canvas.create_line(self.coords_of_parent[0], self.coords_of_parent[1], self.x_coord, self.y_coord)
 
     def get_coords(self): 
         return self.x_coord, self.y_coord
 
-    def shift(self, new_x, new_y): 
-        pass 
+    def swap_location_with(self, other_number_on_canvas): 
+        # removing both numbers from canvas 
+        self.destroy()
+        other_number_on_canvas.destroy()
+        # swapping coords of numbers
+        temp_x_coord = self.x_coord
+        temp_y_coord = self.y_coord
+        self.x_coord, self.y_coord = other_number_on_canvas.x_coord, other_number_on_canvas.y_coord
+        other_number_on_canvas.x_coord, other_number_on_canvas.y_coord = temp_x_coord, temp_y_coord
+        # swapping coords of parents 
+        temp_parent_coords = self.coords_of_parent
+        self.coords_of_parent = other_number_on_canvas.coords_of_parent
+        other_number_on_canvas.coords_of_parent = temp_parent_coords
+        # adding newly adjusted numbers onto canvas
+        self.add_to_canvas()
+        other_number_on_canvas.add_to_canvas()
+        
+    def destroy(self): 
+        self.canvas.delete(self.canvas_image)
+        if self.line_to_parent is not None: 
+            self.canvas.delete(self.line_to_parent)
 
 
 class MaxHeapOnCanvas: 
@@ -79,14 +101,14 @@ class MaxHeapOnCanvas:
         self.level_size += 1
         self.__update_level()
         self.__update_coords_to_add_new()
-        # self.__raise(self.size)
+        self.__raise(self.size)
 
     def __execute_insert(self, number: int): 
         parent_coords = None
         if self.size > 1: 
             parent_coords = self.array[self.size//2].get_coords() 
-        number_on_canvas = NumberOnCanvas(number, self.number_images[number - 1], self.coords_to_add_new[0], self.coords_to_add_new[1])
-        number_on_canvas.add_to_canvas(self.canvas, parent_coords)
+        number_on_canvas = NumberOnCanvas(number, self.number_images[number - 1], self.canvas, 
+                                          self.coords_to_add_new[0], self.coords_to_add_new[1], parent_coords)
         self.array.append(number_on_canvas)
 
     def __update_level(self): 
@@ -107,9 +129,10 @@ class MaxHeapOnCanvas:
             self.coords_to_add_new[0] = next_parent_coords[0] + self.line_side_shift 
 
     def __raise(self, index): 
-        if index > 1 and self.array[index // 2].number < self.array[index].number: 
+        if index > 1 and self.array[index // 2].number < self.array[index].number:  
             self.__swap(index//2, index)
             self.__raise(index//2)
 
     def __swap(self, index1, index2): 
+        self.array[index1].swap_location_with(self.array[index2])
         self.array[index1], self.array[index2] = self.array[index2], self.array[index1]
